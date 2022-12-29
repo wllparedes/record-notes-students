1b20575f04e1808166b49b7498c5e6b0e2c0f8b5
Walin Paredes <walinparedes3010@gmail.com>
2022-12-28 21:35:00 -0500

    1.3 Registro de notas y alumnos - Colorama y Two Spaces

--- registroNotas.py
+++ registroNotas.py
@@ -1,4 +1,8 @@
-# Antes se debió de haber instalado prettytable
+# Antes se debió de haber instalado prettytable y colorama
+# pip install colorama - Colorama
+# pip install prettytable - Prettytable
+
+from colorama import Fore
 from prettytable import PrettyTable
 
 tabla = PrettyTable(['Nombre y Apellidos', 'C. Aprobados', 'C. Desaprobados', 'Condición'])
@@ -7,103 +11,100 @@ cursos = ['Matematica', 'Comunicación', 'Fisica', 'Ciencias Sociales', 'Ciencia
 
 # Función que permite en ingreso del alumno, como tambien de preguntar las notas en respectivos cursos
 
-
 def ingreso():
-    while(True):
-        cursosAprobados = []
-        cursosDesaprobados = []
-        notasAlumnox = []
-        nombre = input('Ingrese nombre/apellidos del alumno: (0 para salir): ')
-        if (nombre == '0'):
+  while(True):
+    cursosAprobados = []
+    cursosDesaprobados = []
+    notasAlumnox = []
+    nombre = input(f'{Fore.LIGHTWHITE_EX}Ingrese nombre/apellidos del alumno: (0 para salir): ')
+    if (nombre == '0'):
+      break
+    c_cur_des = 0
+    c_cur_apro = 0
+    print(f'\n\t.: Ingrese las notas del alumno: {Fore.LIGHTCYAN_EX}{nombre}{Fore.LIGHTWHITE_EX} :.\n')
+    for c in cursos:
+      while (True):
+        try:
+          nota = float(input(f'{Fore.LIGHTWHITE_EX}- Ingrese la nota de {c}: '))
+          if (nota >= 0 and nota <= 20):
+            if (nota >= 13 and nota <= 20):
+              c_cur_apro += 1
+              cursosAprobados.append(c)
+            elif (nota >= 0 and nota <= 12):
+              c_cur_des += 1
+              cursosDesaprobados.append(c)
+            notasAlumnox.append(nota)
+            print(f'{Fore.LIGHTBLUE_EX}✓')
             break
-        c_cur_des = 0
-        c_cur_apro = 0
-        print(f'\n\t.: Ingrese las notas del alumno: {nombre} :.\n')
-        for c in cursos:
-            while (True):
-                try:
-                    nota = float(input(f'- Ingrese la nota de {c}: '))
-                    if (nota >= 0 and nota <= 20):
-                        if (nota >= 13 and nota <= 20):
-                            c_cur_apro += 1
-                            cursosAprobados.append(c)
-                        elif (nota >= 0 and nota <= 12):
-                            c_cur_des += 1
-                            cursosDesaprobados.append(c)
-                        notasAlumnox.append(nota)
-                        print('✓')
-                        break;
-                    else:
-                        print('(!) Nota fuera del rango de 0-20')
-                except ValueError:
-                    print('(!) Ha digitado incorrectamente, vuelva a internarlo')
+          else:
+            print(f'{Fore.RED}(!) Nota fuera del rango de 0-20')
+        except ValueError:
+          print(f'{Fore.RED}(!) Ha digitado incorrectamente, vuelva a internarlo')
 
-        print('(✓) Notas almacenadas correctamente\n')
+    print(f'{Fore.GREEN}(✓) Notas almacenadas correctamente\n')
 
-        # Añade los datos: nombre,ca, cd y condición a la tabla creada anteriormente
-        match (c_cur_des):
-            case 0: tabla.add_row([nombre, c_cur_apro, c_cur_des, '(✓) APROBADO'])
-            case 1 | 2: tabla.add_row([nombre, c_cur_apro, c_cur_des, '(-) VACACIONAL'])
-            case _: tabla.add_row([nombre, c_cur_apro, c_cur_des, '(x) REPITENCIA'])
+    # Añade los datos: nombre,ca, cd y condición a la tabla creada anteriormente
+    match (c_cur_des):
+      case 0: tabla.add_row([nombre, c_cur_apro, c_cur_des, '(✓) APROBADO'])
+      case 1 | 2: tabla.add_row([nombre, c_cur_apro, c_cur_des, '(-) VACACIONAL'])
+      case _: tabla.add_row([nombre, c_cur_apro, c_cur_des, '(x) REPITENCIA'])
 
-        # Añade los datos muy generales a un diccionario donde la clave es el nombre del alumno
-        dictNam_apro_desa[nombre] = cursosAprobados, cursosDesaprobados, notasAlumnox
-
+    # Añade los datos muy generales a un diccionario donde la clave es el nombre del alumno
+    dictNam_apro_desa[nombre] = cursosAprobados, cursosDesaprobados, notasAlumnox
 
 # Función que realiza el promedio de los alumnos ingresado tomando en cuenta las notas ingresadas anteriormente
 def promedio_alumn(dicc):
-    print('\n')
-    for v in dicc:
-        sum_nots = 0
-        listaAlumnNot = dicc[v][2]
-        for n in listaAlumnNot:
-            sum_nots += n
-        promedioFinal = round(sum_nots / len(listaAlumnNot),2)
-        print(f'''(°) Información del alumno: {v}
+  print('\n')
+  for v in dicc:
+    sum_nots = 0
+    listaAlumnNot = dicc[v][2]
+    for n in listaAlumnNot:
+      sum_nots += n
+    promedioFinal = round(sum_nots / len(listaAlumnNot),2)
+    print(f'''(°) Información del alumno: {v}
 (°) Notas: {listaAlumnNot} | Promedio Final: {promedioFinal}''')
-        print('\n')
+    print('\n')
 
 
 # Función que escribe la tabla hecha en un archivo .txt
 def imprimir(table):
-    archivo = open('tablaAlumnos.txt', 'a', encoding='utf-8')
-    archivo.write(str(f'\n{table}'))
-    archivo.close()
-    return '\n(✓) Su tabla ha sido impresa en el archivo .txt con exito\n'
+  archivo = open('tablaAlumnos.txt', 'a+', encoding='utf-8')
+  archivo.write(str(f'\n{table}'))
+  archivo.close()
+  return f'\n{Fore.GREEN}(✓) Su tabla ha sido impresa en el archivo .txt con exito\n'
 
 def limpiar():
-    archivo = open('tablaAlumnos.txt', 'w', encoding='utf-8')
-    archivo.truncate()
-    archivo.close()
-    return '\n(✓) Su archivo .txt ha sido limpiado con exito\n'
-
+  archivo = open('tablaAlumnos.txt', 'r+', encoding='utf-8')
+  archivo.truncate()
+  archivo.close()
+  return f'\n{Fore.GREEN}(✓) Su archivo .txt ha sido limpiado con exito\n'
 
 # Main del programa
 def main_n():
-    while (True):
-        try:
-            print('''\t.: REGISTRO DE NOTAS :.
-\n1. Ingresar notas de alumnos
+  while (True):
+    try:
+      print(f'''\n\t{Fore.LIGHTMAGENTA_EX}.: REGISTRO DE NOTAS :.
+\n{Fore.LIGHTWHITE_EX}1. Ingresar notas de alumnos
 2. Ver tabla de alumnos
 3. Ver Diccionario de alumnos
 4. Ver Promedio General de alumnos y su condición
 5. Imprimir la tabla 
 6. Limpiar archivo .txt
 0. Salir''')
-# Cuando queramos ver el .txt es mejor verlo desde archivos q desde el IDE
-            opc = int(input('\n- Ingrese su opción: '))
-            match (opc):
-                case 0:
-                    print('\n(!!) Saliendo del programa, tenga un buen día :)')
-                    break
-                case 1: ingreso()
-                case 2: print(tabla)
-                case 3: print(dictNam_apro_desa)
-                case 4: promedio_alumn(dictNam_apro_desa)
-                case 5: print(imprimir(tabla))
-                case 6: print(limpiar())
-                case _: print('\n(!) Opción no valida\n')
-        except ValueError:
-            print('\n(!) Opción incorrecta\n')
+      # Cuando queramos ver el .txt es mejor verlo desde archivos q desde el IDE
+      opc = int(input('\n- Ingrese su opción: '))
+      match (opc):
+        case 0:
+          print(f'{Fore.YELLOW}\n(!!) Saliendo del programa, tenga un buen día :){Fore.YELLOW}{Fore.LIGHTWHITE_EX}')
+          break
+        case 1: ingreso()
+        case 2: print(tabla)
+        case 3: print(dictNam_apro_desa)
+        case 4: promedio_alumn(dictNam_apro_desa)
+        case 5: print(imprimir(tabla))
+        case 6: print(limpiar())
+        case _: print(f'{Fore.RED}\n(!) Opción no valida\n')
+    except ValueError:
+      print(f'\n{Fore.RED}(!) Opción incorrecta\n')
 
 main_n()
